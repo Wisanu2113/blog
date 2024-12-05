4707a7a37fe67a29c169a6d2ce094ca6a44eff36
Wisanu Rungkrathoke <wisanu.r@kkumail.com>
2024-12-05 23:12:08 +0700

    Create form and Additon fuction in ArticleController and validate form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :articles
 end
